ef58743d8ea4e66e44dafc45a8f5d652748890f1
Mohamed Diop <yxngbizzy@gmail.com>
2023-03-20 17:17:55 -0700

    add simple resolver

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,11 @@ const yoga = createYoga({
                 hello: String
             }
         `,
-        resolvers: {}
+        resolvers: {
+            Query: {
+                hello: () => "hello world"
+            }
+        }
     })
 })
 
